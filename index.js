78caf2ce5c450f561bc154f4f66141c0eb560180
arihant454 <ajarihant454@gmail.com>
2021-09-18 16:48:56 +0530

    done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var key = "$KEY";
+var key = $KEY;
 $(document).ready(function(){
     $("#submit-btn").click(function(){
         var nameInput = $("#name").val();
         var keyInput = $("#password").val();
-        if (nameInput == config.NAME_KEY && keyInput == key) {
+        if (nameInput == "Arihant" && keyInput == key) {
             alert("welcome");
             window.location.replace("./machine.html")
 
