07768457f13d3bb0fab28df602552adbedac0f3b
arihant454 <ajarihant454@gmail.com>
2021-09-18 15:36:56 +0530

    hiding

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var key = "A9C4light"
+var key = config.SECRET_KEY;
 $(document).ready(function(){
     $("#submit-btn").click(function(){
         var nameInput = $("#name").val();
         var keyInput = $("#password").val();
-        if (nameInput == "Arihant" && keyInput == key) {
+        if (nameInput == config.NAME_KEY && keyInput == key) {
             alert("welcome");
             window.location.replace("./machine.html")
 
