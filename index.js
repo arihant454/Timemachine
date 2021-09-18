a298ce532ba30d75cc16f6633217daade799febb
arihant454 <ajarihant454@gmail.com>
2021-09-18 16:58:46 +0530

    done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-alert("not sleep");
-var key = $KEY;
 $(document).ready(function(){
+        alert("not sleep");
+        var key = $KEY;
     $("#submit-btn").click(function(){
         var nameInput = $("#name").val();
         var keyInput = $("#password").val();
         if (nameInput == "Arihant" && keyInput == key) {
             alert("welcome");
-            window.location.replace("./machine.html")
+            window.location.replace("https://timemachine3.netlify.app/machine.html")
 
         }else{
             alert("Wrong User");
